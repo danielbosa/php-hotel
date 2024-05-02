b692095cefcea541bd2c4c0961b1d83428a411b4
danielbosa <danielbosa95@gmail.com>
2024-05-02 15:16:49 +0200

    Basic

---
+++ index.php
@@ -0,0 +1,109 @@
+<!-- <?php
+include __DIR__ . "/Models/steam.php";
+
+$steams = $db;
+
+include __DIR__ . "/Views/header.php";
+?>
+
+<?php
+include __DIR__ . "/Views/table.php";
+?>
+
+<?php
+include __DIR__ . "/Views/footer.php";
+?> -->
+
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+// foreach($hotels as $hotel){
+//   // echo $hotel['name'] . $hotel['description'] . $hotel['parking'] . $hotel['vote'] . $hotel['distance_to_center'] . "<br>";
+//   "<tr> $hotel['name'] <th>1</th> <td>Mark</td> <td>Otto</td> <td>@mdo</td> </tr>";
+// }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link grity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="js/script.js" type="text/javascript" defer></script>
+    <title>php Hotel</title>
+
+</head>
+<body>
+  
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">Nome</th>
+      <th scope="col">Descrizione</th>
+      <th scope="col">Parcheggio</th>
+      <th scope="col">Voto</th>
+      <th scope="col">Distanza</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+      foreach($hotels as $hotel){
+      // echo $hotel['name'] . $hotel['description'] . $hotel['parking'] . $hotel['vote'] . $hotel['distance_to_center'] . "<br>";
+      echo "<tr> <th>{$hotel['name']}</th> <td>{$hotel['description']}</td> <td>{$hotel['parking']}</td> <td>{$hotel['vote']}</td> <td>{$hotel['distance_to_center']}</td> </tr>";
+      }
+    ?>
+  </tbody>
+</table>
+
+</body>
+</html>
+
+
