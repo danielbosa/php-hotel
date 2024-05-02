ca83506d6bf3325d11fb244e7305b2d687f9a63b
danielbosa <danielbosa95@gmail.com>
2024-05-02 15:27:58 +0200

    Arranged with table.php included in index

---
+++ Views/table.php
@@ -0,0 +1,25 @@
+<?php
+$template = "";
+foreach($cloneHotels as $hotel){
+    $template .= "<tr> <th>{$hotel['name']}</th> <td>{$hotel['description']}</td> <td>{$hotel['parking']}</td> <td>{$hotel['vote']}</td> <td>{$hotel['distance_to_center']}</td> </tr>";
+  }
+
+?>
+
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">Nome</th>
+      <th scope="col">Descrizione</th>
+      <th scope="col">Parcheggio</th>
+      <th scope="col">Voto</th>
+      <th scope="col">Distanza</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?= $template ?>
+  </tbody>
+</table>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,67 +1,10 @@
-<!-- <?php
-include __DIR__ . "/Models/steam.php";
+<?php
+include __DIR__ . "/Models/hotels.php";
 
-$steams = $db;
+$cloneHotels = $hotels;
 
-include __DIR__ . "/Views/header.php";
 ?>
 
-<?php
-include __DIR__ . "/Views/table.php";
-?>
-
-<?php
-include __DIR__ . "/Views/footer.php";
-?> -->
-
-<?php
-
-    $hotels = [
-
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
-
-    ];
-
-// foreach($hotels as $hotel){
-//   // echo $hotel['name'] . $hotel['description'] . $hotel['parking'] . $hotel['vote'] . $hotel['distance_to_center'] . "<br>";
-//   "<tr> $hotel['name'] <th>1</th> <td>Mark</td> <td>Otto</td> <td>@mdo</td> </tr>";
-// }
-
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -82,28 +25,8 @@ include __DIR__ . "/Views/footer.php";
 
 </head>
 <body>
-  
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">Nome</th>
-      <th scope="col">Descrizione</th>
-      <th scope="col">Parcheggio</th>
-      <th scope="col">Voto</th>
-      <th scope="col">Distanza</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
-      foreach($hotels as $hotel){
-      // echo $hotel['name'] . $hotel['description'] . $hotel['parking'] . $hotel['vote'] . $hotel['distance_to_center'] . "<br>";
-      echo "<tr> <th>{$hotel['name']}</th> <td>{$hotel['description']}</td> <td>{$hotel['parking']}</td> <td>{$hotel['vote']}</td> <td>{$hotel['distance_to_center']}</td> </tr>";
-      }
-    ?>
-  </tbody>
-</table>
 
-</body>
-</html>
-
+<?php
+include __DIR__ . "/Views/table.php"
+?>
 
